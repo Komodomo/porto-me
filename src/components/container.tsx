07fe59a074f294e573faa8d7ff559e2912085a5b
Rafi Mahendra <rafmgit@gmail.com>
2024-12-12 10:58:18 +0800

    Update container.tsx

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,6 @@
 export function Container({ children }: { readonly children: React.ReactNode }) {
   return (
-    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="max-w-6xl mx-auto w-full px-4 sm:px-6 lg:px-8">
       {children}
     </div>
   );
