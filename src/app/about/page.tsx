34f167f4ed58442d26f1ee7d894a98759a1441ef
Rafi Mahendra <rafmgit@gmail.com>
2024-12-23 17:05:42 +0800

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,8 +10,8 @@ export default function About() {
       <Container>
         <main className="flex flex-col gap-12 sm:gap-20 py-8 sm:py-20">
           <section className="flex flex-col-reverse md:flex-row items-center justify-between gap-8 px-4 sm:px-0">
-            <div className="flex-1 space-y-4 text-left">
-              <div className="flex gap-4 items-center justify-center mb-8">
+            <div className="flex-1 space-y-2 text-left">
+              <div className="flex gap-4 items-center justify-center mb-8 mt-12 md:mt-4 lg:mt-2">
                 <div className="w-1/3 h-[200px] flex items-center justify-center relative">
                   <img
                     src="https://i.pinimg.com/736x/76/62/54/76625494ac72bc481ce598f0eb31f1ea.jpg"
